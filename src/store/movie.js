242e04227345a213f48b89c411b43333bdbd1137
jc <wnwlcks123@gmail.com>
2023-09-12 14:28:41 +0900

    영화 목록 더 가져오기

--- src/store/movie.js
+++ src/store/movie.js
@@ -3,20 +3,22 @@ import { Store } from "../core/heropy";
 const store = new Store({
   searchText: '',
   page: 1,
+  pageMax: 1,
   movies: []
 })
 
 export default store
 export const searchMovies = async page => {
+  store.state.page = page;
   if (page === 1){
-    store.state.page = 1;
     store.state.movies = [];
   }
   //7035c60c
   const res = await fetch(`https://omdbapi.com?apikey=52c458e9&s=${store.state.searchText}&page=${page}`)
-  const { Search } = await res.json();
+  const { Search, totalResults } = await res.json();
   store.state.movies = [
     ...store.state.movies,
     ...Search
   ]
+  store.state.pageMax = Math.ceil(Number(totalResults) / 10)
 }
