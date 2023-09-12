1a7c64bb960e911bbe02978f81da94e57f1127fc
jc <wnwlcks123@gmail.com>
2023-09-12 13:30:20 +0900

    MovieList 컴포넌트

--- src/store/movie.js
+++ src/store/movie.js
@@ -8,8 +8,15 @@ const store = new Store({
 
 export default store
 export const searchMovies = async page => {
+  if (page === 1){
+    store.state.page = 1;
+    store.state.movies = [];
+  }
   //7035c60c
   const res = await fetch(`https://omdbapi.com?apikey=52c458e9&s=${store.state.searchText}&page=${page}`)
-  const json = await res.json();
-  console.log(json)
+  const { Search } = await res.json();
+  store.state.movies = [
+    ...store.state.movies,
+    ...Search
+  ]
 }
